ea0c56bb6a3e2b6d23c89875af5596dfdd00a193
al1k24 <al1k24@icloud.com>
2019-12-27 20:12:40 +0200

    Initial Commit

---
+++ Start WebPower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Start WebPower
+//
+//  Created by Алик on 12/27/19.
+//  Copyright © 2019 ALIK MOLDOVANU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
