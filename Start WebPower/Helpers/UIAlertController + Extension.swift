348ff6c215fa136cf96103e0a5bac6d9ea4fa3d9
al1k24 <al1k24@icloud.com>
2020-01-08 21:47:45 +0200

    Update showAlert function

--- Start WebPower/Helpers/UIAlertController + Extension.swift
+++ Start WebPower/Helpers/UIAlertController + Extension.swift
@@ -10,11 +10,12 @@ import UIKit
 
 extension UIViewController {
     
-    func showAlert(with title: String, and message: String) {
+    func showAlert(with title: String, and message: String, completion: (() -> Void)? = nil) {
+        
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alertController.addAction(okAction)
-        present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: completion)
     }
 }
