b5621b4c0e6f08655df6c3198d42c1d6963a3ea2
al1k24 <al1k24@icloud.com>
2020-01-08 18:24:58 +0200

    Add Alert function

---
+++ Start WebPower/Helpers/UIAlertController + Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIAlertController + Extension.swift
+//  Start WebPower
+//
+//  Created by Алик on 1/8/20.
+//  Copyright © 2020 ALIK MOLDOVANU. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showAlert(with title: String, and message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
